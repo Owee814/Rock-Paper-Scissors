27ca2e29b96471048ca84de623b29bad79fc0e71
Owee814 <mailowen814@gmail.com>
2022-10-30 08:46:22 -0700

    Java ifs function

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-const rock = document.getElementsByClassName("Rock");
-const paper = document.getElementsByClassName("Paper");
-const scissors = document.getElementsByClassName("Scissors");
-const otherAnswer = document.getElementsByClassName("Other-Answer");
+var rock = document.querySelector(".rock");
+var paper = document.querySelector(".paper");
+var scissors = document.querySelector(".scissors");
+var otherAnswer = document.querySelector(".other-answer");
+var playerAnswer = document.querySelector(".player-answer");
+var winLose = document.querySelector(".win-lose");
+let answers = ["Rock", "Paper", "Scissors"];
+
+if (rock) {
+    rock.addEventListener("click", function() {
+        playerAnswer.textContent = "You: Rock";
+        makeOtherAnswer();
+    });
+};
+
+if (paper) {
+    paper.addEventListener("click", function() {
+        playerAnswer.textContent = "You: Paper";
+        makeOtherAnswer();
+    });
+};
+
+if (scissors) {
+    scissors.addEventListener("click", function() {
+        playerAnswer.textContent = "You: Scissors";
+        makeOtherAnswer();
+    });
+};
