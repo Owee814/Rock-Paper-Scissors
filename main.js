c2c3091c12b21bdb7086a024f31b8546feee08e2
Owee814 <mailowen814@gmail.com>
2022-10-29 17:21:56 -0700

    java variables

---
+++ main.js
@@ -0,0 +1,4 @@
+const rock = document.getElementsByClassName("Rock");
+const paper = document.getElementsByClassName("Paper");
+const scissors = document.getElementsByClassName("Scissors");
+const otherAnswer = document.getElementsByClassName("Other-Answer");
